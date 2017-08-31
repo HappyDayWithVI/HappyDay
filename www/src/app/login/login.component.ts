724a819aea0aee400af8ef37094249e2e81b4ef8
Haoua <haouasou@gmail.com>
2017-08-31 03:11:16 +0300

    init display google login button

--- www/src/app/login/login.component.ts
+++ www/src/app/login/login.component.ts
@@ -11,13 +11,41 @@ import { routerTransition } from '../router.animations';
 export class LoginComponent implements OnInit {
 
     constructor(public router: Router) {
+
+    }
+
+    public loadScript() {
+        console.log('preparing to load...')
+        let node = document.createElement('script');
+        node.src = 'https://apis.google.com/js/platform.js';
+        node.type = 'text/javascript';
+        node.async = true;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('head')[0].appendChild(node);
     }
 
     ngOnInit() {
+     console.log('preparing to load...')
+        let node = document.createElement('script');
+        node.src = 'https://apis.google.com/js/platform.js';
+        node.type = 'text/javascript';
+        node.async = true;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('head')[0].appendChild(node);
     }
 
     onLoggedin() {
         localStorage.setItem('isLoggedin', 'true');
     }
+
+    onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
+
 
 }
